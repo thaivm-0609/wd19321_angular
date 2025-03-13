4aa8518181fd0d77c3cef93e30f77560255928a2
thaivm-0609 <vuongthai95@gmail.com>
2025-03-13 09:20:42 +0700

    feat: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wd19321_angular';
+  title = 'thaivm2';
+  listStudents = [
+    {
+      maSv: 'PH123456',
+      ten: 'thaivm2',
+      nganh: 'Web',
+      anh: 'https://picsum.photos/200',
+      namSinh: '1900' 
+    },
+    {
+      maSv: 'PH123467',
+      ten: 'thaivm3',
+      nganh: 'Marketing',
+      anh: 'https://picsum.photos/200',
+      namSinh: '1901' 
+    },
+    {
+      maSv: 'PH123478',
+      ten: 'thaivm4',
+      nganh: 'Design',
+      anh: 'https://picsum.photos/200',
+      namSinh: '1902' 
+    }
+  ]
 }
