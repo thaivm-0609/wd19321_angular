acced37fe92c7c03ffd533020ba81ac607498869
thaivm-0609 <vuongthai95@gmail.com>
2025-03-18 09:05:48 +0700

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+//PATH KHÔNG ĐƯỢC BẮT ĐẦU BẰNG DẤU /(slash)
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    },
+    {
+        path: 'create',
+        component: CreateComponent
+    },
+    { //truyền params qua url /:tenParam
+        path: 'edit/:id',
+        component: EditComponent
+    },
+    { //luôn nằm cuối cùng ở trong route
+        path: '**',
+        component: NotFoundComponent
+    }
+];
